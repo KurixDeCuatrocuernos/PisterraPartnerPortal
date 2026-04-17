599dbd007ffcc83763d0f320dd7edd778e17d163
KurixDeCuatrocuernos <168125007+KurixDeCuatrocuernos@users.noreply.github.com>
2026-04-17 19:43:37 +0200

    Creación de la vista de Facturas
    1. Se modificó menus.xml para que no estuviese metido dentro de CRM, sino que formase un módulo aparte. Afecta a:
     - views/menus.xml -> Se añadieron records para cada menuitem para interactuar
    
    2. Se creó la vista de las facturas que creó el usuario. Afectó a:
     - views/factura_simple_template.xml (se creó)
     - controller/portal.py -> se añadió el controlador mis_facturas
     - __manifest__.py -> Se añadió la dependencia website para el enrutamiento y el template: views/factura_simple_template.xml
    
    3. Se creó la vista para ver una factura en detalle. Afectó a:
     - views/factura_detallada_template.xml (se creó)
     - controller/portal.py -> se añadió el controlador factura_en_detalle
     - __manifest__.py -> Se añadió el template views/factura_detallada_template.xml

--- __manifest__.py
+++ __manifest__.py
@@ -3,12 +3,14 @@
     'version': '0.1',
     'summary': 'Portal de bookings y clientes',
     # Módulos necesarios para funcionar
-    'depends': ['base', 'web', 'portal', 'sale', 'stock', 'account'],
+    'depends': ['base', 'web', 'portal', 'website', 'sale', 'stock', 'account'],
     'data': [ 
             'security/ir.model.access.csv',
             'views/unload_booking_views.xml',
             'views/menus.xml',
             'views/portal_templates.xml',
+            'views/factura_simple_template.xml',
+            'views/factura_detallada_template.xml',
         ],
         'installable': True,
         'application': True,
--- controllers/portal.py
+++ controllers/portal.py
@@ -17,10 +17,35 @@ Ejemplo:
 from odoo import http
 from odoo.http import request
 
+# Enrutamiento al módulo principal 
 class Portal(http.Controller):
-    @http.route('/my/bookings', auth='user')
+    
+    # Ruta para ver los envíos
+    @http.route('/pisterra/bookings', auth='user')
     def my_bookings(self):
         bookings = request.env['pisterra.unload.booking'].search([])
         return request.render('pisterra_partner_portal.portal_bookings_template', {
             'bookings': bookings
+        })
+    
+    # Ruta para ver las facturas
+    @http.route(['/pisterra/invoices', '/pisterra/invoices/'], auth='user', website=True)
+    def mis_facturas(self):
+        # Recogemos las facturas del usuario que sean enviadas y no estén canceladas
+        facturas = request.env['account.move'].search([
+            ('partner_id', '=', request.env.user.partner_id.id),
+            ('move_type', '=', 'out_invoice'),
+            ('state', '=', 'posted')
+        ])
+        return request.render('pisterra_partner_portal.factura_simple_template', {
+            'facturas': facturas,
+        })
+        # Devolvemos las facturas a facturas_template.xml
+   
+    # Ruta para ver una factura en detalle   
+    @http.route('/pisterra/invoices/<int:invoice_id>', auth='user', website=True)
+    def factura_en_detalle(self, invoice_id):
+        factura = request.env['account.move'].sudo().browse(invoice_id)
+        return request.render('pisterra_partner_portal.factura_detallada_template', {
+            'f': factura
         })
